86d2611b08129cb535b1e1b25a9148f0732211dd
heojua <80086434+heojua@users.noreply.github.com>
2021-09-20 15:11:11 +0900

    Add files via upload

---
+++ codeup1048.java
@@ -0,0 +1,13 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class codeup1048 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.printf("%d",a<<b);
+    }
+    
+}
---
+++ codeup1053.java
@@ -0,0 +1,17 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class codeup1053 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a==0){
+            System.out.println("1");
+            }
+        if(a==1){
+            System.out.println("0");
+        }
+    }
+    
+}
