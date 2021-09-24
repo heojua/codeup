6f2ac17fb46423db01541f52119115d221cda0f9
heojua <80086434+heojua@users.noreply.github.com>
2021-09-24 16:25:27 +0900

    [기초-비트단위논리연산]
    [기초-비트단위논리연산]

---
+++ codeup1059.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class codeup1059 {
+    public static void main(String[] srgs){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b;
+        b=-(a+1);
+        System.out.println(b);
+    }
+}
